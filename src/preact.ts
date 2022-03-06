fd88db5cbefacd7df3b5469ec1358bbd512e2a47
Clemens Akens <clebert@me.com>
2022-03-06 13:07:56 +0100

    Fix SWC Preact integration

--- src/preact.ts
+++ src/preact.ts
@@ -4,7 +4,12 @@ import {swc} from './swc.js';
 import {typescript} from './typescript.js';
 
 export const preact = (): readonly AnyFileStatement[] => [
-  mergeContent(swc.configFile, {jsc: {parser: {tsx: true}}}),
+  mergeContent(swc.configFile, {
+    jsc: {
+      parser: {tsx: true},
+      transform: {react: {importSource: `preact`, runtime: `automatic`}},
+    },
+  }),
 
   mergeContent(typescript.configFile, {
     compilerOptions: {jsx: `react-jsx`, jsxImportSource: `preact`},
